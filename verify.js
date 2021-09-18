5169c83073899d5f3ad3aee0472418d3f40d9da6
grade82021 <90674062+grade82021@users.noreply.github.com>
2021-09-18 15:32:06 +0530

    Update verify.js

--- verify.js
+++ verify.js
@@ -4,7 +4,7 @@ var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 var password = document.getElementById("password").value;
 if ( username == "sidharth.parmar" && password == "sidparmar8"||username == "kush.kundalia" && password == "rockeeee1"|| username=="dhyan.thakkar"&&password=="animeguy20000"||username == "tirth.khunt" && password == "tirthk01"||username == "vasu.rakholiya" && password=="vasur18"||username=="yuvraj.dadhaniya"&&password=="yuvrajd02"||username=="krish.kotadiya"&&password=="krishk10"){
-alert ("Hello " + username + "Thanks for logging in, click on ok to continue");
+alert ("Hello " + username + "Thanks for coming, click on ok to continue");
 window.location = "addition-grade8-gibbonware-maintrue.html"; 
 return false;
 }
