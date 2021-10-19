58bf33fdb61c1186ba78e92ca53ae5506653e5ad
grade82021 <90674062+grade82021@users.noreply.github.com>
2021-10-19 18:26:29 +0530

    Update verify.js

--- verify.js
+++ verify.js
@@ -3,7 +3,7 @@ function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 var password = document.getElementById("password").value;
-if ( username == "saloni.rawal" && password == "anything123456"||username == "kush.kundalia" && password == "kushk17"|| username=="dhyan.thakkar"&&password=="animeguy2000"||username == "tirth.khunt" && password == "tirthk92"||username == "vasu.rakholiya" && password=="vasur18"||username=="yuvraj.dadhaniya"&&password=="yuvrajd02"||username=="krish.kotadiya"&&password=="krishk10"||username=="tanya.punjabi"&&password=="tanyap88"||username=="prisha.popat"&&password=="prishap20"){
+if ( username == "kush.kundalia" && password == "kushk17"|| username=="dhyan.thakkar"&&password=="animeguy2000"||username == "tirth.khunt" && password == "tirthk92"||username == "vasu.rakholiya" && password=="vasur18"||username=="yuvraj.dadhaniya"&&password=="yuvrajd02"||username=="krish.kotadiya"&&password=="krishk10"||username=="tanya.punjabi"&&password=="tanyap88"||username=="prisha.popat"&&password=="prishap20"){
 alert ("Hello " + username + " Thanks for logging in, click on ok to continue");
 window.location = "addition-grade8-gibbonware-maintrue.html"; 
 return false;
@@ -24,54 +24,3 @@ return false;
 }
 }
 }
-function validate(){
-    var username = document.getElementById("username").value;
-    var password = document.getElementById("password").value;
-    var password = document.getElementById("password").value;
-    if (username=="admin.dhyan"&& password=="qwcvas12"){
-    prompt ("Hello Dhyan, Welcome to your management console for the website, Click on OK to continue");
-    window.location = "admin.html"; 
-    return false;
-    }
-    else{
-    attempt --;
-    alert("Please Re-Check Your Credentials");
-    
-    
-        
-     if( attempt == 0){
-    document.getElementById("username").disabled = true;
-    document.getElementById("password").disabled = true;
-    document.getElementById("submit").disabled = true;
-    alert ("Your login rights have been suspended");
-    
-    return false;
-    }
-    }
-    }
-
-    function validate(){
-        var username = document.getElementById("username").value;
-        var password = document.getElementById("password").value;
-        var password = document.getElementById("password").value;
-        if (username=="admin.kush"&& password=="rockeeee1"){
-        alert ("Hello Kush, Welcome to your management console for the website, Click on OK to continue");
-        window.location = "admin.html"; 
-        return false;
-        }
-        else{
-        attempt --;
-        alert("Please Re-Check Your Credentials");
-        
-        
-            
-         if( attempt == 0){
-        document.getElementById("username").disabled = true;
-        document.getElementById("password").disabled = true;
-        document.getElementById("submit").disabled = true;
-        alert ("Your login rights have been suspended");
-        
-        return false;
-        }
-        }
-        }
