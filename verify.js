c6df583109dacbec16e8ad6ff7050f60acd6bfa7
grade82021 <90674062+grade82021@users.noreply.github.com>
2021-10-19 18:33:39 +0530

    Update verify.js

--- verify.js
+++ verify.js
@@ -3,7 +3,7 @@ function validate(){
 var username = document.getElementById("username").value;
 var password = document.getElementById("password").value;
 var password = document.getElementById("password").value;
-if ( username == "kush.kundalia" && password == "kushk17"|| username=="dhyan.thakkar"&&password=="animeguy2000"||username == "tirth.khunt" && password == "tirthk92"||username == "vasu.rakholiya" && password=="vasur18"||username=="yuvraj.dadhaniya"&&password=="yuvrajd02"||username=="krish.kotadiya"&&password=="krishk10"||username=="tanya.punjabi"&&password=="tanyap88"||username=="prisha.popat"&&password=="prishap20"){
+if ( username == "kush.kundalia" && password == "kushk17"|| username=="dhyan.thakkar"&&password=="animeguy2000"||username == "tirth.khunt" && password == "tirthk92"||username == "vasu.rakholiya" && password=="vasur18"||username=="yuvraj.dadhaniya"&&password=="yuvrajd02"||username=="krish.kotadiya"&&password=="krishk10"||username=="tanya.punjabi"&&password=="tanyap88"||username=="saloni.rawal"&&password=="anything123456"){
 alert ("Hello " + username + " Thanks for logging in, click on ok to continue");
 window.location = "addition-grade8-gibbonware-maintrue.html"; 
 return false;
